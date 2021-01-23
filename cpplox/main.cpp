3ad265092555afc2c59f9d6cd4bbc6624b1ed60e
Morgan <hx801@nyu.edu>
2021-01-24 00:32:13 +0800

    Initial Commit

---
+++ cpplox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpplox
+//
+//  Created by Morgan Xu on 1/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
